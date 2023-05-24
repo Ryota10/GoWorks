6f13d86e6fd1e5c5a301e13439a514b3323016da
Gatsby <liugatsby17@gmail.com>
2023-05-24 15:29:31 +0800

    init commit

---
+++ src/main.go
@@ -0,0 +1,16 @@
+/*
+ * @Author       : liu-j-d
+ * @Date         : 2023-05-24 10:22:40
+ * @LastEditTime : 2023-05-24 10:22:41
+ * @LastEditors  : liu-j-d
+ * @Description  :
+ * @FilePath     : \GoWorks\src\main.go
+ */
+package main
+
+import "fmt"
+
+func main() {
+	/* 这是我的第一个简单的程序 */
+	fmt.Println("Hello, World!")
+}
